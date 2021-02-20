a7844bba3fb92871571aa627ba509cc2aa2d0611
Ahmet H Kaya <hudaiahmet@gmail.com>
2021-02-20 18:27:59 +0300

    Dynamic Button

--- src/main/java/com/ahk/frame/controller/CustomerController.java
+++ src/main/java/com/ahk/frame/controller/CustomerController.java
@@ -1,4 +1,47 @@
 package com.ahk.frame.controller;
 
-public class CustomerController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerController implements Initializable {
+    @FXML
+    BorderPane customerBorder;
+    @FXML
+    ScrollPane scrollPaneButtons;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        VBox box = new VBox();
+
+        int a = 0;
+        for(int i = 0;i<11;i++){
+            HBox box2 = new HBox();
+            for (int j = 0;j<10;j++){
+
+                Button button = new Button(""+(a+1));
+                box2.setMargin(button,new Insets(5,5,5,5));
+                button.setMaxHeight(48);
+                button.setMaxWidth(48);
+                button.setMinHeight(48);
+                button.setMinWidth(48);
+                button.setPadding(new Insets(10,10,10,10));
+                box2.getChildren().add(button);
+                a++;
+
+            }
+            box.getChildren().add(box2);
+        }
+        scrollPaneButtons.setContent(box);
+    }
 }
